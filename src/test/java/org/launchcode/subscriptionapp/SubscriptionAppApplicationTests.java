1d54e00cefe081aa3d83c6db6bceeba9b6cf8f5f
John Gibbs <johngibbs110@gmail.com>
2020-08-12 21:41:05 -0500

    Initial commit.

---
+++ src/test/java/org/launchcode/subscriptionapp/SubscriptionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.subscriptionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
