7a92750dfafcc7b0dbd7b0ed6f0269975e54c0aa
John Gibbs <johngibbs110@gmail.com>
2020-08-28 07:20:14 -0500

    Fixed navbar and Thymeleaf titles

--- src/main/java/org/launchcode/subscriptionapp/controllers/HelloController.java
+++ src/main/java/org/launchcode/subscriptionapp/controllers/HelloController.java
@@ -1,13 +1,15 @@
 package org.launchcode.subscriptionapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("title", "Home");
         return "homepage";
     }
 
