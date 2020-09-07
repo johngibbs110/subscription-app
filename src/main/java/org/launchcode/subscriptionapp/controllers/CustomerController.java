1f86d2754bd8a0cb7ca9124a394a67fd91a98aeb
John Gibbs <johngibbs110@gmail.com>
2020-09-07 08:56:35 -0500

    Wired up edit customer button to landing page

--- src/main/java/org/launchcode/subscriptionapp/controllers/CustomerController.java
+++ src/main/java/org/launchcode/subscriptionapp/controllers/CustomerController.java
@@ -1,16 +1,17 @@
 package org.launchcode.subscriptionapp.controllers;
 
-import org.launchcode.subscriptionapp.data.CustomerData;
 import org.launchcode.subscriptionapp.data.CustomerRepository;
 import org.launchcode.subscriptionapp.models.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.Optional;
 
 @Controller
