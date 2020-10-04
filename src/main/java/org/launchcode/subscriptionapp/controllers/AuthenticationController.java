3491a324a637f12b84b26359d4c74120c729c230
John Gibbs <johngibbs110@gmail.com>
2020-10-04 11:24:39 -0500

    Register form complete and working

--- src/main/java/org/launchcode/subscriptionapp/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/subscriptionapp/controllers/AuthenticationController.java
@@ -2,10 +2,18 @@ package org.launchcode.subscriptionapp.controllers;
 
 import org.launchcode.subscriptionapp.data.OwnerRepository;
 import org.launchcode.subscriptionapp.models.Owner;
+import org.launchcode.subscriptionapp.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -34,6 +42,42 @@ public class AuthenticationController {
 
     private static void setUserInSession(HttpSession session, Owner owner) {
         session.setAttribute(userSessionKey, owner.getId());
+    }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO, Errors errors, HttpServletRequest request, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        Owner existingOwner = ownerRepository.findByUsername(registerFormDTO.getUsername());
+
+        if (existingOwner != null) {
+            errors.rejectValue("username", "username.alreadyexists", "An owner with that username already exists");
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+            model.addAttribute("title", "Register");
+        }
+
+        Owner newOwner = new Owner(registerFormDTO.getUsername(), registerFormDTO.getPassword());
+        ownerRepository.save(newOwner);
+        setUserInSession(request.getSession(), newOwner);
+
+        return "redirect:";
     }
 
 }
